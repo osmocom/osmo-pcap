dd1389c3d3e69fbb55669df9c9f63567bea8522a
Harald Welte <laforge@osmocom.org>
2021-04-24 13:00:41 +0200

    use telnet_init_dynif() to allow VTY bind to non-loopack address
    Prior to this patch, you could configure 'bind 1.2.3.4' in the
    config file, but the telnet interface still binds to 127.0.0.1.
    
    Change-Id: I9b86b2cf6949917c55ea15277619cfa2b745185d

--- src/osmo_client_main.c
+++ src/osmo_client_main.c
@@ -259,7 +259,7 @@ int main(int argc, char **argv)
 
 	osmo_tls_init();
 
-	rc = telnet_init(tall_cli_ctx, NULL, OSMO_VTY_PORT_PCAP_CLIENT);
+	rc = telnet_init_dynif(tall_cli_ctx, NULL, vty_get_bind_addr(), OSMO_VTY_PORT_PCAP_CLIENT);
 	if (rc < 0) {
 		LOGP(DCLIENT, LOGL_ERROR, "Failed to bind telnet interface\n");
 		exit(1);

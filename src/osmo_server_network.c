osmocom/osmo-pcap

commit d94e1ffcb75f7b2390138d7eb3b95b0f879b1e91

pcap-server: New vty command 'no max-file-size'
This new VTY command allows disabling the max-file-size feature.

Change-Id: I54f1f0e2976ea235e92a1c527fa80f18ae166992

src/osmo_server_network.c
@@ -217,6 +217,8 @@ static bool check_restart_pcap_max_size(struct osmo_pcap_conn *conn, const struc
 {
 	off_t cur;
 
+	if (pcap_server->max_size_enabled)
+		return false;
 	cur = lseek(conn->local_fd, 0, SEEK_CUR);
 	if (cur + data->len <= conn->server->max_size)
 		return false;

include/osmo-pcap/osmo_pcap_server.h
@@ -131,6 +131,7 @@ struct osmo_pcap_server {
 	char *base_path;
 	mode_t permission_mask;
 	off_t max_size;
+	bool max_size_enabled;
 	int max_snaplen;
 
 	/* statistics */

src/osmo_server_main.c
@@ -281,7 +281,8 @@ int main(int argc, char **argv)
 	INIT_LLIST_HEAD(&pcap_server->conn);
 	pcap_server->base_path = talloc_strdup(pcap_server, "./");
 	pcap_server->permission_mask = 0440;
-	pcap_server->max_size = 1073741824;
+	pcap_server->max_size = 1073741824; /* 1024^3, 1GB **/
+	pcap_server->max_size_enabled = true;
 	pcap_server->max_snaplen = DEFAULT_SNAPLEN;
 
 	if (vty_read_config_file(config_file, NULL) < 0) {

src/osmo_server_vty.c
@@ -93,8 +93,10 @@ static int config_write_server(struct vty *vty)
 		vty_out(vty, " server ip %s%s", pcap_server->addr, VTY_NEWLINE);
 	if (pcap_server->port > 0)
 		vty_out(vty, " server port %d%s", pcap_server->port, VTY_NEWLINE);
-	vty_out(vty, " max-file-size %llu%s",
-		(unsigned long long) pcap_server->max_size, VTY_NEWLINE);
+	if (pcap_server->max_size_enabled)
+		vty_out(vty, " max-file-size %llu%s", (unsigned long long)pcap_server->max_size, VTY_NEWLINE);
+	else
+		vty_out(vty, " no max-file-size%s", VTY_NEWLINE);
 	if (pcap_server->max_snaplen != DEFAULT_SNAPLEN)
 		vty_out(vty, " server max-snaplen %d%s", pcap_server->max_snaplen, VTY_NEWLINE);
 	if (pcap_server->zmq_port > 0)
@@ -198,6 +200,16 @@ DEFUN(cfg_server_max_size,
       "Maximum file size for a trace\n" "Filesize in bytes\n")
 {
 	pcap_server->max_size = strtoull(argv[0], NULL, 10);
+	pcap_server->max_size_enabled = true;
+	return CMD_SUCCESS;
+}
+
+DEFUN(cfg_server_no_max_size,
+      cfg_server_no_max_size_cmd,
+      "no max-file-size",
+      NO_STR "Maximum file size for a trace\n")
+{
+	pcap_server->max_size_enabled = false;
 	return CMD_SUCCESS;
 }
 
@@ -564,6 +576,7 @@ void vty_server_init(void)
 	install_element(SERVER_NODE, &cfg_server_ip_cmd);
 	install_element(SERVER_NODE, &cfg_server_port_cmd);
 	install_element(SERVER_NODE, &cfg_server_max_size_cmd);
+	install_element(SERVER_NODE, &cfg_server_no_max_size_cmd);
 	install_element(SERVER_NODE, &cfg_server_max_snaplen_cmd);
 	install_element(SERVER_NODE, &cfg_server_zmq_ip_port_cmd);
 	install_element(SERVER_NODE, &cfg_no_server_zmq_ip_port_cmd);

commit eeb3d67adda27231f964c5404918db9b572e1d8c

pcap-server: Fix [no] max-file-size regression
Fix typo ending in inversed logic in max-file-size VTY command.

Fixes: d94e1ffcb75f7b2390138d7eb3b95b0f879b1e91
Change-Id: I8f41a045a036301daf8d95535e7043f79db7075e

src/osmo_server_network.c
@@ -217,7 +217,7 @@ static bool check_restart_pcap_max_size(struct osmo_pcap_conn *conn, const struc
 {
 	off_t cur;
 
-	if (pcap_server->max_size_enabled)
+	if (!pcap_server->max_size_enabled)
 		return false;
 	cur = lseek(conn->local_fd, 0, SEEK_CUR);
 	if (cur + data->len <= conn->server->max_size)